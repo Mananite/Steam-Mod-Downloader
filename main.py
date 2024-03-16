1f0ccc9e3c25736d6d791b07c24f5477a40824ab
Mana <88516116+Mananite@users.noreply.github.com>
2024-03-16 12:38:49 +0100

    Still has test code, but it works, just change the app_id

--- main.py
+++ main.py
@@ -1,52 +1,62 @@
-import re, os, requests, time
-from bs4 import BeautifulSoup
+import re, os, time
  
+print("""
+███████╗████████╗███████╗ █████╗ ███╗   ███╗	███╗   ███╗ ██████╗ ██████╗		 ██████╗  ██████╗ ██╗	██╗███╗   ██╗██╗	  ██████╗  █████╗ ██████╗ ███████╗██████╗ 
+██╔════╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║	████╗ ████║██╔═══██╗██╔══██╗	██╔══██╗██╔═══██╗██║	██║████╗  ██║██║	 ██╔═══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
+███████╗   ██║   █████╗  ███████║██╔████╔██║	██╔████╔██║██║   ██║██║  ██║	██║  ██║██║   ██║██║ █╗ ██║██╔██╗ ██║██║	 ██║   ██║███████║██║  ██║█████╗  ██████╔╝
+╚════██║   ██║   ██╔══╝  ██╔══██║██║╚██╔╝██║	██║╚██╔╝██║██║   ██║██║  ██║	██║  ██║██║   ██║██║███╗██║██║╚██╗██║██║	 ██║   ██║██╔══██║██║  ██║██╔══╝  ██╔══██╗
+███████║   ██║   ███████╗██║  ██║██║ ╚═╝ ██║	██║ ╚═╝ ██║╚██████╔╝██████╔╝	██████╔╝╚██████╔╝╚███╔███╔╝██║ ╚████║███████╗╚██████╔╝██║  ██║██████╔╝███████╗██║  ██║
+╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝	 ╚═╝	╚═╝	 ╚═╝ ╚═════╝ ╚═════╝	 ╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝	 
+			  
+█░█░█ █ █▀█
+▀▄▀▄▀ █ █▀▀
+""")
+
+placeholder_app_id = 0
+app_id = 0
 mods = []
-app_id = []
-headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Max-Age': '3600',
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-    }
 
 with open("mods.txt","r+") as f:
-    lines = f.readlines()
-    for i in range(len(lines)):
-        only_number = re.search(r'\d+',str(lines[i])).group()
-        mods.append(only_number)
+	lines = f.readlines()
+		
+	for i in range(len(lines)):
 
+		if not lines[i].startswith("https://steamcommunity.com/sharedfiles"):
+			pass
 
-for i in range(len(lines)):
-  soup = BeautifulSoup(requests.get(lines[i], headers).content,"html.parser")
-  for link in soup.find_all('a'):
-    y=link.get('href')
-    if "https://steamcommunity.com/app/" in y:
-      only_number = re.search(r'\d+',str(y)).group()
-      app_id.append(only_number)
-      break
+		if lines[i].startswith("https://steamcommunity.com/workshop/browse/"):
+			app_id: re.search(r'\bappid=(\d+)+',str(lines[i]))
+			print(app_id)
+
+			if app_id != placeholder_app_id and placeholder_app_id != 0:
+				print("This doesnt support multiple games, go into the code and change stuff if you want to support it and make a fork, exiting")
+				time.sleep("15")
+				os.system("exit")
+			
+			placeholder_app_id = app_id
+			continue
+
+		only_number = re.search(r"\bid=(\d+)",str(lines[i]))
+		mods.append(str(only_number))
+		
+
+if app_id == 0 or None:
+	app_id = str(input("Appid: "))
 
 
 if os.path.exists("q.txt"): #mostly a just in case tbh
   os.remove("q.txt")
-
   
-if len(mods) != len(app_id):
-  print("Something went terribly wrong and the number of mods differs with the number of games")
-  time.sleep(4)
-  exit()
-
 
 with open("q.txt","w+") as f:
-    f.write("login anonymous")
-    f.write("\n")
-    for i in range(len(mods)):
-        f.write(f"workshop_download_item " f"{app_id[i-1]} {mods[i-1]}")
-        f.write("\n")
-    f.close()  
+	f.write("login anonymous")
+	f.write("\n")
+	for i in range(len(mods)+1):
+		f.write(f"workshop_download_item " f"{app_id} {mods[i-1]}")
+		f.write("\n")
+	f.write("exit")
+	f.close()
 
 
-os.system("steamcmd.exe +runscript q.txt")
-os.remove("q.txt")
-os.system("exit")
+os.system("steamcmd.exe +runscript q.txt +stop")
+os.remove("q.txt") #doesnt actually work since steamcmd wont close
